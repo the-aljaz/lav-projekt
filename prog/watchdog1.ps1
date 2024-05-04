e3a65988a6e44b9682cde392082ca1232302d0b3
Jure <grcar.jure1@gmail.com>
2024-05-04 12:51:05 +0200

    fix watchdoging

--- prog/watchdog1.ps1
+++ prog/watchdog1.ps1
@@ -5,7 +5,7 @@ while ($true) {
     $wd0pid = Get-Content wd0pid.txt
     $wd0pid = $wd0pid.Trim()    # odstrani kere koli presledke ali tabulatorje
     try {
-        $wd0proc = Get-Process -Id $wd0pid -ErrorAction SilentlyContinue    # proba, ce je watchdog0 se ziv
+        $wd0proc = Get-Process -Id $wd0pid -ErrorAction Stop    # proba, ce je watchdog0 se ziv
     }
     catch {   # ce proces ne obstaja
         Start-Process -FilePath "powershell.exe" -ArgumentList "-WindowStyle Hidden -Command & './watchdog0.ps1'"
