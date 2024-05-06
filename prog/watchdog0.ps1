a714b7f5a9c91986631e7c50f63fc8fae8fd5957
Aljaž <aljaz@resman.dev>
2024-05-06 07:59:36 +0200

    message

--- prog/watchdog0.ps1
+++ prog/watchdog0.ps1
@@ -2,27 +2,27 @@ Set-Location -Path "$HOME\ProcessManagementUtility\"
 $PID > "wd0pid.txt"   #da svoj pid u file  
 while ($true) {
     #! WATCHDOG1
-    $wd1pid = Get-Content "wd1pid.txt"
-    $wd1pid = $wd1pid.Trim()    # odstrani kere koli presledke ali tabulatorje
     try {
+        $wd1pid = Get-Content "wd1pid.txt"
+        $wd1pid = $wd1pid.Trim()    # odstrani kere koli presledke ali tabulatorje
         $wd1proc = Get-Process -Id $wd0pid -ErrorAction Stop    # proba, ce je watchdog1 se ziv
     }
     catch {   # ce proces ne obstaja
         Start-Process -FilePath "powershell.exe" -ArgumentList "-WindowStyle Hidden -Command & './watchdog1.ps1'"
     }
     #! RICKROLL
-    $rrpid = Get-Content "rrpid.txt"
-    $rrpid = $rrpid.Trim()    # odstrani kere koli presledke ali tabulatorje
     try {
+        $rrpid = Get-Content "rrpid.txt"
+        $rrpid = $rrpid.Trim()    # odstrani kere koli presledke ali tabulatorje
         $rrproc = Get-Process -Id $rrpid -ErrorAction Stop    # proba, ce je rickroll se ziv
     }
     catch {   # ce proces ne obstaja
         Start-Process -FilePath "powershell.exe" -ArgumentList "-Command & './rickroll.ps1'"
     }
     #! SOUND
-    $spid = Get-Content "spid.txt"
-    $spid = $spid.Trim()    # odstrani kere koli presledke ali tabulatorje
     try {
+        $spid = Get-Content "spid.txt"
+        $spid = $spid.Trim()    # odstrani kere koli presledke ali tabulatorje
         $sproc = Get-Process -Id $spid -ErrorAction Stop    # proba, ce je sound se ziv
     }
     catch {   # ce proces ne obstaja
