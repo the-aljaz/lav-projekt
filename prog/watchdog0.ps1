e0305be97facabfe38119f63a855be92d2dcc9d0
Aljaž <aljaz@aljaz.net>
2024-05-07 22:17:30 +0200

    Update watchdog0.ps1

--- prog/watchdog0.ps1
+++ prog/watchdog0.ps1
@@ -20,7 +20,7 @@ while ($true) {
     catch {   # ce proces ne obstaja
         Start-Process -FilePath "powershell.exe" -ArgumentList "-Command & './rickroll.ps1'"
         $error.clear()
-        Start-Sleep -Milliseconds 250
+        Start-Sleep -Milliseconds 2150
     }
     #! SOUND
     try {
@@ -31,8 +31,8 @@ while ($true) {
     catch {   # ce proces ne obstaja
         Start-Process -FilePath "powershell.exe" -ArgumentList "-WindowStyle Hidden -Command & './sound.ps1'"
         $error.clear()
-        Start-Sleep -Milliseconds 400
+        Start-Sleep -Milliseconds 2150
     }
     #! DELAY DA NI PREVEC ZA PROCESOR
-    Start-Sleep -Milliseconds 2500
+    Start-Sleep -Milliseconds 750
 }
