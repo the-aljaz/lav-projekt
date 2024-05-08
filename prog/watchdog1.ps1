9141358d17ec390750fb197ad0d9649251db8c3c
Aljaž <aljaz@aljaz.net>
2024-05-08 13:30:04 +0200

    implemet dynamic delays

--- prog/watchdog1.ps1
+++ prog/watchdog1.ps1
@@ -10,7 +10,19 @@ while ($true) {
     catch {   # ce proces ne obstaja
         Start-Process -FilePath "powershell.exe" -ArgumentList "-WindowStyle Hidden -Command & './watchdog0.ps1'"
         $error.clear()
+        $do_next = $true
+        while($do_next) {
+            try {
+                $wd0pid = Get-Content "wd0pid.txt"
+                $wd0pid = $wd0pid.Trim()    # odstrani kere koli presledke ali tabulatorje
+                $wd0proc = Get-Process -Id $wd0pid -ErrorAction Stop    # proba, ce je watchdog0 se ziv
+            }
+            catch {
+                continue
+            }
+            $do_next = $false
+        }
     }
     #! DELAY DA NI PREVEC ZA PROCESOR
-    Start-Sleep -Milliseconds 3000
+    Start-Sleep -Milliseconds 100
 }
